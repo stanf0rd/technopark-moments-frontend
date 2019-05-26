f3de3914593f3f343d28aa589525b5e6fe24ca93
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-26 18:30:29 +0300

    Layout contents Headline

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import Headline from '../Headline';
+
+import './Layout.pcss';
+
 export default function Layout() {
-  return <div>Hello Layout!</div>;
+  return (
+    <div>
+      <Headline />
+    </div>
+  );
 }
