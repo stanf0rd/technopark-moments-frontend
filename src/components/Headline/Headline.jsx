189dd941bc3e7c89cb10fb6bc101ad790eadb1aa
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-26 18:30:07 +0300

    basic headline component

---
+++ src/components/Headline/Headline.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Headline.pcss';
+
+export default class Headline extends Component {
+  render() {
+    return (
+      <nav className="headline">
+        Headline
+      </nav>
+    )
+  }
+}
