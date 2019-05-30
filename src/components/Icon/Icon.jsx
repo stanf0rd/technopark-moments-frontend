92e2cc115b4cdb316c1ff36d794b3aa488e8f7ef
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-30 21:59:21 +0300

    icon can be wrapped into link if href set

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import './Icon.pcss';
 
-export default function Icon({ type }) {
-  const className = `icon icon__${type}`;
+export default function Icon({ type, href }) {
+  const className = `icon__${type}`;
+  if (href) {
+    return (
+      <a className="icon__link-wrapper" href={href}>
+        <div className={`icon ${className}`} />
+      </a>
+    );
+  }
+
   return (
-    <div className={className} />
+    <div className={`icon ${className}`} />
   );
 }
