5cac8b97c7b70d770fe256de7de7607ccb6174d9
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-30 22:45:25 +0300

    small search-lens icon :3

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 import './Search.pcss';
 
+import Icon from '../Icon';
+
 export default function Search() {
   return (
     <div className="search">
       {/* <input className="search__input" /> */}
-      <div className="search__button" role="button">Поиск</div>
+      <div className="search__button" role="button">
+        <span className="search__icon">
+          <Icon img="search" size="small" />
+        </span>
+        <span className="search__text">
+          Поиск
+        </span>
+      </div>
     </div>
   );
 }
