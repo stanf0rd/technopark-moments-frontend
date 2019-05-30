4e814728d55df4b4c4eb6793d75f594884314830
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-30 22:00:17 +0300

    links in iconblock

--- src/components/Iconblock/Iconblock.jsx
+++ src/components/Iconblock/Iconblock.jsx
@@ -7,9 +7,9 @@ import Icon from '../Icon';
 export default function IconBlock() {
   return (
     <div className="iconblock">
-      <Icon type="explore" />
-      <Icon type="like" />
-      <Icon type="profile" />
+      <Icon type="explore" href="/explore" />
+      <Icon type="like" href="/like" />
+      <Icon type="profile" href="/profile" />
     </div>
   );
 }
