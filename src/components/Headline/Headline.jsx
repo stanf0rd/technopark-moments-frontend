ae03e95111419cd28055820839c6e5ae327ddd4a
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-30 20:28:09 +0300

    Headline includes Logo, Search and Iconblock

--- src/components/Headline/Headline.jsx
+++ src/components/Headline/Headline.jsx
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import './Headline.pcss';
 
-export default class Headline extends Component {
-  render() {
-    return (
-      <nav className="headline">
-        Headline
-      </nav>
-    )
-  }
+import Logo from '../Logo';
+import Search from '../Search';
+import Iconblock from '../Iconblock';
+
+export default function Headline() {
+  return (
+    <nav className="headline">
+      <Logo />
+      <Search />
+      <Iconblock />
+    </nav>
+  );
 }
