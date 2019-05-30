9ea9d686492d64b977677c421311757c80c51f3c
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-30 20:27:03 +0300

    icon component used in Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './Logo.pcss';
 
+import Icon from '../Icon';
+
 export default function Logo() {
   return (
     <a className="logo" href="/">
-      <span className="logo__icon" />
+      <Icon type="instagram" />
       <div className="logo__separator" />
       <span className="logo__lettering" />
     </a>
