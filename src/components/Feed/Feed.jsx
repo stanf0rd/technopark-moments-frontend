8c677987d89c7dda4c984713b71db4b7d4248527
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-31 01:45:12 +0300

    Feed and Moment component basics

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Feed.pcss';
+
+import Moment from '../Moment';
+
+export default function Main() {
+  return (
+    <div className="feed">
+      <Moment />
+    </div>
+  );
+}
---
+++ src/components/Moment/Moment.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Moment(props) {
+  return (
+    <div>Hello World!</div>
+  );
+}
