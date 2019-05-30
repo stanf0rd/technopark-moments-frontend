2f3604f56426a8d0e3c9e5e9b754a8c3f4e53fba
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-30 20:25:33 +0300

    search mock component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Search.pcss';
+
+export default function Search() {
+  return (
+    <div className="search">
+      {/* <input className="search__input" /> */}
+      <div className="search__button" role="button">Поиск</div>
+    </div>
+  );
+}
