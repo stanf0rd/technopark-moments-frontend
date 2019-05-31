3ba65fcfddea3e573e463a4268ba4a55c8f4ef36
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-31 21:16:33 +0300

    clickable Icon with callback

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 
 import './Icon.pcss';
 
-export default function Icon({ img, href, size }) {
+export default function Icon({
+  img,
+  href,
+  size,
+  onClick,
+}) {
   const className = `icon__${img} icon__${size || 'middle'}`;
   if (href) {
     return (
       <a className="icon__link-wrapper" href={href}>
         <div className={`icon ${className}`} />
       </a>
+    );
+  }
+
+  if (onClick) {
+    return (
+      <div
+        role="button"
+        tabIndex={0}
+        onClick={onClick}
+        onKeyPress={onClick}
+        className={`icon icon_clickable ${className}`}
+      />
     );
   }
 
