f4ba3532e82d36501dcd4e39c3bf14e99be340fb
Dmitry Paliy <d.paliy@corp.mail.ru>
2019-05-31 20:30:04 +0300

    Moment component with Header

--- src/components/Moment/Moment.jsx
+++ src/components/Moment/Moment.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import './Moment.pcss';
+
+import Avatar from '../Avatar';
+
 export default function Moment(props) {
+  const { author, location } = props;
+  const locationHref = `/location/${location}`;
   return (
-    <div>Hello World!</div>
+    <article className="moment">
+      <header className="moment__header">
+        <Avatar
+          src={author.avatar}
+          size="small"
+          href={author.link}
+        />
+        <div className="moment__info">
+          <a className="moment__author" href={author.link}>{author.name}</a>
+          <a className="moment__location" href={locationHref}>{location}</a>
+        </div>
+      </header>
+    </article>
   );
 }
